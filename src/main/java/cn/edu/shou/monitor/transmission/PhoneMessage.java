3e369cd8e490c7e5f76702a25659b1234bff4d46
Shoujue Xu <lightxusj@126.com>
2016-04-23 16:18:54 +0800

    alarm sms
    alarm sms

--- src/main/java/cn/edu/shou/monitor/transmission/PhoneMessage.java
+++ src/main/java/cn/edu/shou/monitor/transmission/PhoneMessage.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ import java.util.Map;
 /**
  * Created by light on 2016/4/14.
  */
-@RestController
+@Repository
 public class PhoneMessage {
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -24,21 +25,24 @@ public class PhoneMessage {
     @TargetDataSource(name = "predict")
     public String sendMessage(String hostid){  // result 中Array size 为2
 
-//        ZbxHostServiceImpl zbxHost = new ZbxHostServiceImpl();
-//        JSONObject host = zbxHost.getHostIdAndName(hostid);
-//        JSONArray hostResult = host.getJSONArray("result");
-//        String mame = hostResult.getJSONObject(0).getString("name");
-
         ZbxTriggerServiceImpl trigger = new ZbxTriggerServiceImpl();
         JSONObject triggerRs = trigger.getTriggerForSms(hostid);
         JSONArray result = triggerRs.getJSONArray("result");  //结果信息
+        String description = result.getJSONObject(0).getString("description");
+        String priority = result.getJSONObject(0).getString("priority");
         String content = null;
         if(!result.toString().equals("[]")){
-            String selectSql = "SELECT hosts FROM predict.zbx_hosts where host_id = '"+ hostid+"';";
-            List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
-            list = jdbcTemplate.queryForList(selectSql);
-            String name = list.get(0).get("hosts").toString();
-            content = "机房主机出现报警，请尽快处理。详细信息为：{主机编号：" + name + ",报警内容: "+result.toString();
+            String selectSqlName = "SELECT hosts FROM predict.zbx_hosts where host_id = '"+ hostid+"';";
+            String selectSqlDesc = "SELECT desc_zh FROM predict.alarm_description where desc_en = '"+ description+"';";
+
+            List<Map<String,Object>> listName = new ArrayList<Map<String,Object>>();
+            listName = jdbcTemplate.queryForList(selectSqlName);
+            String name = listName.get(0).get("hosts").toString();
+
+            List<Map<String,Object>> listdesc = new ArrayList<Map<String,Object>>();
+            listdesc = jdbcTemplate.queryForList(selectSqlDesc);
+            String desc_zh = listdesc.get(0).get("desc_zh").toString();
+            content = "机房主机出现报警，请尽快处理。详细信息为：主机编号：" + name + ",报警内容: "+ desc_zh + ",警报等级"+ priority+"。";
         }
         return content;
     }
